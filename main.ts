62a6395e419cb0adb392ccaf7fa8e36e2821cd66
Nicolas HILAIRE <nicolas.hilaire@gmail.com>
2023-02-11 21:14:16 +0100

    wheel and robot movement in progress

--- main.ts
+++ main.ts
@@ -1,18 +1,7 @@
-function stop () {
-    pins.servoWritePin(AnalogPin.P3, 90)
-    pins.servoWritePin(AnalogPin.P4, 90)
-}
-function reculer () {
-    pins.servoWritePin(AnalogPin.P3, 40)
-    pins.servoWritePin(AnalogPin.P4, 140)
-}
-function tourner_gauche () {
-    pins.servoWritePin(AnalogPin.P3, 90)
-    pins.servoWritePin(AnalogPin.P4, 40)
-}
-function avancer () {
-    pins.servoWritePin(AnalogPin.P3, 140)
-    pins.servoWritePin(AnalogPin.P4, 40)
-}
-robot.configurer_roue(Roue.Gauche, AnalogPin.P0)
-pins.servoSetPulse(AnalogPin.P3, 1500)
+robot.configurer_roue(Roue.Gauche, AnalogPin.P3)
+robot.tourner_roue(Roue.Gauche, Sens.Avant, 50)
+basic.forever(function () {
+    robot.deplacer(RobotDirection.Gauche, 50)
+    basic.pause(2000)
+    robot.stop()
+})
--- robot.ts
+++ robot.ts
@@ -6,11 +6,22 @@ let wheel_pins: Array<AnalogPin>;
 // default values
 wheel_pins = [AnalogPin.P3, AnalogPin.P4];
 
-enum Direction {
+enum Sens {
     //% block="avant"
     Avant = 0,
     //% block="arriere"
     Arriere
+}
+
+enum RobotDirection {
+    //% block="avant"
+    Avant = 0,
+    //% block="arriere"
+    Arriere,
+    //% block="gauche"
+    Gauche = 0,
+    //% block="droite"
+    Droite,
 }
 
 enum Roue {
@@ -32,17 +43,22 @@ namespace robot {
     }
 
     /**
-     * Demande à une roue de tourner
+     * Demande à la roue de tourner
      * @param roue : roue à faire tourner
-     * @param direction : direction de la rotation
+     * @param sens : Sens de la rotation
      * @param vitesse : vitesse de rotation de 0 à 100
      */
-    //% block
-    export function tourner_roue(roue: Roue, direction: Direction, vitesse: number): void {
-        let pwmOffset = vitesse * 10
-        // 100 % de vitesse se déroule sur 1 ms
-        if (((roue == Roue.Gauche) && (direction == Direction.Avant)) ||
-            ((roue == Roue.Droite) && (direction == Direction.Arriere))) {
+    //% block="Tourner la roue $roue vers l'$sens à la vitesse $vitesse"
+    //% vitesse.defl=50
+    export function tourner_roue(roue: Roue, sens: Sens, vitesse: number): void {
+        //la position par defaut est à 1500 µs
+        //on ajoute 500 pour booster artificielement la vitesse par rapport au poids du robot
+        let pwmOffset = vitesse * 5 + 500
+        // 100 % de vitesse se déroule sur 0.5 ms
+        // en dessous de 0.5ms le robot ne bouge pas
+
+        if (((roue == Roue.Gauche) && (sens == Sens.Arriere)) ||
+            ((roue == Roue.Droite) && (sens == Sens.Avant))) {
                 pwmOffset *= -1
         }
 
@@ -58,4 +74,40 @@ namespace robot {
         pins.servoSetPulse(wheel_pins[roue], 1500)
     }
 
+    /**
+     * Demande au robot de se déplacer
+     * @param direction : direction à prendre
+     */
+    //% block
+    //% vitesse.defl=50
+    export function deplacer(direction: RobotDirection, vitesse: number): void {
+        switch (direction) {
+            case RobotDirection.Droite :
+                tourner_roue(Roue.Gauche, Sens.Avant, vitesse)
+                tourner_roue(Roue.Droite, Sens.Arriere, vitesse)
+                break
+            case RobotDirection.Gauche:
+                tourner_roue(Roue.Gauche, Sens.Arriere, vitesse)
+                tourner_roue(Roue.Droite, Sens.Avant, vitesse)
+                break
+            case RobotDirection.Avant:
+                tourner_roue(Roue.Gauche, Sens.Avant, vitesse)
+                tourner_roue(Roue.Droite, Sens.Avant, vitesse)
+                break
+            case RobotDirection.Arriere:
+                tourner_roue(Roue.Gauche, Sens.Arriere, vitesse)
+                tourner_roue(Roue.Droite, Sens.Arriere, vitesse)
+                break
+        }
+
+    }
+
+    /**
+     * Demande au robot de s'arrêter
+     */
+    //% block
+    export function stop(): void {
+        stopper_roue(Roue.Gauche)
+        stopper_roue(Roue.Droite)
+    }
 }
